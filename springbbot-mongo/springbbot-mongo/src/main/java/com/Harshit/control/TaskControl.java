a2438e21416aa95bcaca51fdf4c25515130f9eb2
Harshit <82029204+harshit2392000@users.noreply.github.com>
2023-03-30 16:24:47 +0530

    Add files via upload

---
+++ springbbot-mongo/springbbot-mongo/src/main/java/com/Harshit/control/TaskControl.java
@@ -0,0 +1,41 @@
+package com.Harshit.control;
+
+import com.Harshit.model.Task;
+import com.Harshit.service.TaskService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/tasks")
+public class TaskControl {
+    @Autowired
+    private TaskService service;
+    @PutMapping
+    public Task createTask(@RequestBody Task task){
+        return  service.addTask(task);
+    }
+
+    @GetMapping
+    public List<Task> getTasks(){
+        return service.findAllTask();
+    }
+
+   @GetMapping("/{id}")
+    public Task getTask(@PathVariable String id){
+        return service.getTaskById(id);
+
+    }
+
+    @GetMapping("/name/{name}")
+    public List<Task> getTaskUsingName(@PathVariable String name){
+        return service.getTaskByName(name);
+    }
+
+   @DeleteMapping("/{id}")
+    public String deleteTask(@PathVariable String id){
+        return service.deleteTask(id);
+    }
+
+}
---
+++ springbbot-mongo/springbbot-mongo/src/main/java/com/Harshit/service/TaskService.java
@@ -0,0 +1,40 @@
+package  com.Harshit.service;
+
+import com.Harshit.model.Task;
+import com.Harshit.repository.TaskRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class TaskService {
+    @Autowired
+    private TaskRepository repository;
+
+    public Task addTask(Task task){
+        return repository.save(task);
+
+
+    }
+
+    public List<Task> findAllTask(){
+       return repository.findAll();
+    }
+
+    public Task getTaskById(String id){
+
+        return repository.findById(id).get();
+
+    }
+
+    public List<Task> getTaskByName(String name){
+        return repository.findByName(name);
+    }
+
+    public String deleteTask(String id){
+        repository.deleteById(id);
+        return id + "task delete from dashboard";
+    }
+
+}
